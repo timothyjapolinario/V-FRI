31b6bb142529b6d3b8172440e16f474b25fb6233
timothyjapolinario <timothyjapolinario@gmail.com>
2023-11-16 21:47:48 +0800

    Add message to main page

--- app/MainContent.tsx
+++ app/MainContent.tsx
@@ -1,9 +1,12 @@
 const MainContent = () => {
   return (
     <main>
-      <div>
-        <h1>Valenzuela Flood Risk Index</h1>
-      </div>
+      <h1 className="w-full bg-white text-black p-5 text-2xl">
+        Valenzuela Flood Risk Index
+      </h1>
+      <p>Start ko na ayusin tom hehe.</p>
+      <p>Deployment lang ginawa ko ngayon, thanks ulit</p>
+      <p>-Timothy Pogi</p>
     </main>
   );
 };
