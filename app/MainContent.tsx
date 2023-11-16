1cadb80e87942737227c9177c8bec860c8ad9b7e
timothyjapolinario <timothyjapolinario@gmail.com>
2023-11-16 20:59:38 +0800

    Initial Commit

---
+++ app/MainContent.tsx
@@ -0,0 +1,11 @@
+const MainContent = () => {
+  return (
+    <main>
+      <div>
+        <h1>Valenzuela Flood Risk Index</h1>
+      </div>
+    </main>
+  );
+};
+
+export default MainContent;
---
+++ app/page.tsx
@@ -0,0 +1,6 @@
+import Image from "next/image";
+import MainContent from "./MainContent";
+
+export default function Home() {
+  return <MainContent />;
+}
