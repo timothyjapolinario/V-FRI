d14dfee57dbd181c64890d087c03e22af459b974
timothyjapolinario <timothyjapolinario@gmail.com>
2023-11-18 17:31:57 +0800

    Add link to file manager

--- components/ServiceTable.tsx
+++ components/ServiceTable.tsx
@@ -1,14 +1,18 @@
+import Link from "next/link";
 import ServiceLink from "./ServiceLink";
 
 const ServiceTable = () => {
   return (
     <div className="flex flex-wrap gap-4 w-full sm:grid sm:grid-cols-2 lg:grid-cols-3">
-      <div className="w-[100%] max-w-[300px]">
-        <ServiceLink
-          urlIcon="/icons/document-icon.png"
-          shortDescription="Manage your files here."
-        />
-      </div>
+      <Link href={"/files"}>
+        <div className="w-[100%] max-w-[300px]">
+          <ServiceLink
+            urlIcon="/icons/document-icon.png"
+            shortDescription="Manage your files here."
+          />
+        </div>
+      </Link>
+
       <div className="w-[100%] max-w-[300px]">
         <ServiceLink
           urlIcon="/icons/flood-icon.png"
