63f0105270514cd79286d3c6420f4ce90fae0f32
timothyjapolinario <timothyjapolinario@gmail.com>
2023-11-18 13:30:13 +0800

    Fix mobile UI responsiveness

--- components/ServiceLink.tsx
+++ components/ServiceLink.tsx
@@ -4,8 +4,8 @@ type Prop = {
 };
 const ServiceLink = ({ urlIcon, shortDescription }: Prop) => {
   return (
-    <div className="bg-gray-200 shadow-sm border-2 border-solid border-black flex w-full p-2 items-center gap-5 rounded-md cursor-pointer">
-      <img src={urlIcon} className="h-[70px]" />
+    <div className="h-full bg-gray-200 shadow-sm border-2 border-solid border-black flex w-full p-2 items-center gap-2 rounded-md cursor-pointer">
+      <img src={urlIcon} className="h-[60px]" />
       <p className="font-bold">{shortDescription}</p>
       <button className="text-blue-500 justify-self-end">{">"}</button>
     </div>
--- components/ServiceTable.tsx
+++ components/ServiceTable.tsx
@@ -2,11 +2,17 @@ import ServiceLink from "./ServiceLink";
 
 const ServiceTable = () => {
   return (
-    <div>
-      <div className="w-[250px]">
+    <div className="flex flex-wrap gap-4 w-full md:grid grid-cols-3">
+      <div className="w-[100%] max-w-[300px]">
         <ServiceLink
           urlIcon="/icons/document-icon.png"
           shortDescription="Manage your files here."
+        />
+      </div>
+      <div className="sm:w-[100%] max-w-[300px]">
+        <ServiceLink
+          urlIcon="/icons/document-icon.png"
+          shortDescription="Calculate flood risk index."
         />
       </div>
     </div>
