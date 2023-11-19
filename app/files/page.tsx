4164985c621eef45f8b1266520cfd7f4cfc2b77e
timothyjapolinario <timothyjapolinario@gmail.com>
2023-11-19 13:56:20 +0800

    Add downloading of file and styling

--- app/files/page.tsx
+++ app/files/page.tsx
@@ -8,6 +8,27 @@ const Files = () => {
   const { cloudFiles, isLoading, isMutating, trigger } = getAllCloudFiles();
   const [selectedFile, setSelectedFile] = useState<File>();
   const [isUploading, setIsUploading] = useState(false);
+
+  const onClickUpload = () => {
+    if (selectedFile) {
+      const existingFile = cloudFiles.find(
+        (cf) => cf.fileName === selectedFile.name
+      );
+      if (existingFile) {
+        alert("File Name already exist. Select other file or change file name");
+        return;
+      }
+      setIsUploading(true);
+      uploadFile(selectedFile).then(() => {
+        console.log("tapos na mag upload");
+        trigger().then(() => {
+          console.log("tapos na mg fetch");
+          setIsUploading(false);
+        });
+      });
+    }
+  };
+
   return (
     <div>
       <h1>Files</h1>
@@ -22,16 +43,7 @@ const Files = () => {
       />
       <button
         className="bg-green-500 p-1 rounded-md"
-        onClick={() => {
-          if (selectedFile) {
-            setIsUploading(true);
-            uploadFile(selectedFile).then(() => {
-              trigger().then(() => {
-                setIsUploading(false);
-              });
-            });
-          }
-        }}
+        onClick={onClickUpload}
         disabled={isUploading}
       >
         Upload!
@@ -52,10 +64,16 @@ const Files = () => {
           return (
             <div
               key={"cf" + index}
-              className="border-black border-solid border-2 w-fit"
+              className="flex border-black border-solid border-2 w-full justify-between my-1"
             >
-              <p>Filename: {cf.fileName}</p>
-              <p>ContentType: {cf.contentType}</p>
+              <div className="flex items-center">
+                <img src={`/icons/${cf.contentType}.png`} />
+                <a href={cf.downloadUrl}>
+                  <p className="text-blue-500 underline">
+                    Filename: {cf.fileName}
+                  </p>
+                </a>
+              </div>
             </div>
           );
         })}
